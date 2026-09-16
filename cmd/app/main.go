cfd16f898613fa8666766091da92f014d38ccef3
sponez <ganzuk1998@gmail.com>
2026-09-17 03:19:01 +0400

    Made Job struct

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/sponez/job-manager/internal/domain/job"
+)
 
 func main() {
-	fmt.Println("Job manager started.")
+	var jobs []job.Job
+	var jobsNum = rand.Intn(10)
+
+	for i := 0; i < jobsNum; i++ {
+		job := job.Job{
+			ID:     i + 1,
+			Name:   fmt.Sprintf("Job %v", i+1),
+			Status: job.StatusPending,
+		}
+
+		jobs = append(jobs, job)
+	}
+
+	for _, job := range jobs {
+		fmt.Printf(
+			"%v | %v | %v\n",
+			job.ID,
+			job.Name,
+			job.Status,
+		)
+	}
 }
